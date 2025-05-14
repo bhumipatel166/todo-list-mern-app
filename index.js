377a6890afd7fa8d3f9150da5f30f6d8f8e7bfc0
Tapan Patel <tapanpatel8690@Tapans-MacBook-Air.local>
2025-05-14 22:49:11 +0530

    resolve error

--- index.js
+++ index.js
@@ -8,20 +8,30 @@ app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const { name } = require('ejs');
+const uri='mongodb+srv://bhumi:test%40123@todolist.ijxvwuf.mongodb.net/?retryWrites=true&w=majority&appName=todoList';
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
-
-mongoose.connect('mongodb+srv://bhumi:test%40123@todolist.ijxvwuf.mongodb.net/?retryWrites=true&w=majority&appName=todoList', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    // Define your schemas and models here
-    const tryschema = new mongoose.Schema({ name: String, priority: String });
-    const item = mongoose.model('task', tryschema);
-
-    // Start your server or perform database operations here
-  })
-  .catch(err => {
-    console.error('Database connection error:', err);
-  });
-
+const tryschema = new mongoose.Schema({
+     name: String,
+     priority: String
+});
+const item = mongoose.model('task', tryschema);
+const todo = new item({ 
+    name: "create some videos",
+    priority: "High"
+});
 //todo.save();
 
 app.get('/', async (req, res) => {
